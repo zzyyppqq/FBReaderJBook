1d7a4899f05af4dbe3ee14492d4b7b894e1a77e6
zhangyipeng <zhangyipeng@maxrocky.com>
2016-01-15 17:36:13 +0800

    Initial commit

---
+++ fBReader/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/fBReader/Users/zhangyipeng/Downloads/GITHUB/FBReaderJ1/fBReader/src/main/jni/DeflatingDecompressor/DeflatingDecompressor.o.d
@@ -0,0 +1,35 @@
+/Users/zhangyipeng/Downloads/GITHUB/FBReaderJ1/fBReader/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/fBReader//Users/zhangyipeng/Downloads/GITHUB/FBReaderJ1/fBReader/src/main/jni/DeflatingDecompressor/DeflatingDecompressor.o: \
+ /Users/zhangyipeng/Downloads/GITHUB/FBReaderJ1/fBReader/src/main/jni/DeflatingDecompressor/DeflatingDecompressor.cpp \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/string.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/malloc.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/xlocale.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/zlib.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/zconf.h \
+ /Users/zhangyipeng/sdk/ndk-bundle/sources/cxx-stl/system/include/new \
+ /Users/zhangyipeng/sdk/ndk-bundle/sources/cxx-stl/system/include/cstddef
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/string.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/malloc.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/xlocale.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/zlib.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/zconf.h:
+
+/Users/zhangyipeng/sdk/ndk-bundle/sources/cxx-stl/system/include/new:
+
+/Users/zhangyipeng/sdk/ndk-bundle/sources/cxx-stl/system/include/cstddef:
